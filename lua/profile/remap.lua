79c8c0acd6a5db0c5942453e63278385f57d0c60
Ardian-Kovanxhi <akovanxhi@gmail.com>
2024-06-10 15:57:09 -0400

    split and save shortcuts added

--- lua/profile/remap.lua
+++ lua/profile/remap.lua
@@ -1,3 +1,9 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>tr", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>tt", vim.cmd.term)
+vim.keymap.set("n", "<leader>ws", vim.cmd.split)
+vim.keymap.set("n", "<leader>wv", vim.cmd.vsplit)
+vim.keymap.set("n", "<leader>ws", vim.cmd.split)
+vim.keymap.set("n", "<leader>wq", vim.cmd.wq)
+vim.keymap.set("n", "<leader>w", vim.cmd.w)
+vim.keymap.set("n", "<leader>q", vim.cmd.q)
