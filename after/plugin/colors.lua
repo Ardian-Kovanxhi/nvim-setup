3d6de1c028667c6aa441304bacbe6a16ddd8beae
Ardian-Kovanxhi <akovanxhi@gmail.com>
2025-06-14 21:21:45 -0400

    transparent background

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,9 +1,18 @@
 function ColorMyPencils(color)
 	color = color or 'vscode'
 	vim.cmd.colorscheme(color)
-	vim.cmd.set("number")
+	vim.cmd.set("relativenumber")
 	vim.cmd.set('shiftwidth=4')
 	vim.cmd.set('tabstop=4')
+	vim.cmd [[
+  		highlight Normal guibg=none ctermbg=none
+		highlight NonText guibg=none ctermbg=none
+		highlight LineNr guibg=none ctermbg=none
+		highlight NormalNc guibg=none ctermbg=none
+		highlight NormalSB guibg=none ctermbg=none
+		highlight SignColumn guibg=none ctermbg=none
+		highlight FoldColumn guibg=none ctermbg=none
+	]]
 end
 
 ColorMyPencils()
